cdd817da9ac5802d78c755dacac4bbc9fc56f403
ariunzulm <munkhbatt.ariunzul@gmail.com>
2025-12-24 21:54:56 +0800

    sidebar

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const playButton$ = document.getElementById("playButton");
+const pauseButton$ = document.getElementById("pauseButton");
+const video$ = document.querySelector(".video-container");
 
-playButton$.addEventListener('click', playFunction())
-
+pauseButton$.style.display = "none";
+const playVideo = () => {
+  video$.play();
+  playButton$.style.display = "none";
+  pauseButton$.style.display = "block";
+};
+console.log(playVideo);
+const pauseVideo = () => {
+  if (!video$.play()) {
+    pauseButton$.style.display = "none";
+    playButton$.style.display = "block";
+  }
+};
+console.log(pauseVideo);
+playButton$.addEventListener("click", playVideo);
+pauseButton$.addEventListener("click", pauseVideo);
